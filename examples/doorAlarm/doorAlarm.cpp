f46127d41465cfa67007f6e4ad40391dc211c65b
root <root@beaglebone.localdomain>
2018-02-12 20:08:19 +0000

    distance with speaker

---
+++ examples/doorAlarm/doorAlarm.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <iostream>
+#include <unistd.h>
+
+#include "ip_connection.h"
+#include "bricklet_distance_us.h"
+#include "bricklet_piezo_speaker.h"
+
+#define HOST "192.168.178.52"
+#define PORT 4223
+#define UID_DISTANCE "zqf" // Change XYZ to the UID of your Distance US Bricklet
+#define UID_SPEAKER "C84"
+
+int main(void) {
+    // Create IP connection
+    IPConnection ipcon;
+    ipcon_create(&ipcon);
+
+    // Create device object
+    DistanceUS dus;
+    distance_us_create(&dus, UID_DISTANCE, &ipcon);
+
+    PiezoSpeaker ps;
+    piezo_speaker_create(&ps, UID_SPEAKER, &ipcon);
+
+    // Connect to brickd
+    if(ipcon_connect(&ipcon, HOST, PORT) < 0) {
+        fprintf(stderr, "Could not connect\n");
+        return 1;
+    }
+    // Don't use device before ipcon is connected
+
+    while(true){
+    	// Get current distance value
+    	uint16_t distance;
+    	if(distance_us_get_distance_value(&dus, &distance) < 0) {
+        	fprintf(stderr, "Could not get distance value, probably timeout\n");
+        	return 1;
+    	}
+
+    	printf("Distance Value: %u\n", distance);
+
+	sleep(1);
+    }
+
+
+
+
+    printf("Press key to exit\n");
+    getchar();
+    distance_us_destroy(&dus);
+    ipcon_destroy(&ipcon); // Calls ipcon_disconnect internally
+    return 0;
+}
